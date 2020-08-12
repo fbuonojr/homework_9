1731811a7c28aec7d4e45d7877c2c86674fa5cae
Frank Buono <fbuonojr@fordham.edu>
2020-08-12 15:56:21 -0400

    added inquirer prompt

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,8 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 // array of questions for user
 const questions = [
     [
@@ -59,8 +61,12 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    return inquirer.prompt(questions);
 }
 
 // function call to initialize program
-init();
+init().then(function(answers){
+    const md = generateMarkdown(answers);
+
+    return writeFileAsync("createdReadMe.md", md);
+});
