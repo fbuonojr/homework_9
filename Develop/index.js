e22f32d56323883c163548cf666a3c557c656817
Frank Buono <fbuonojr@fordham.edu>
2020-08-12 15:49:18 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,51 @@
 const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 // array of questions for user
 const questions = [
-
+    [
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the project title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a short description of the project?"
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "What licenses should your project have?"
+        },
+        {
+            type: "input",
+            name: "food",
+            message: "What is your favorite food?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub Username"
+        },
+        {
+            type: "input",
+            name: "linkedin",
+            message: "Enter your LinkedIn URL."
+        }
+    ]
 ];
 
 // function to write README file
