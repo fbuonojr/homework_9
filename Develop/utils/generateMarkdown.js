6de95fd7d4ab1f4a1be07c2f16185eb61cda24cb
Frank Buono <fbuonojr@fordham.edu>
2020-08-12 16:12:39 -0400

    generate markdown file complete

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,13 +2,51 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Name
+
+  GitHub Username: ${data.name}
+
+  Email Address: ${data.email}
+
   ## Description
 
   ${data.description}
 
   ## Table of Contents
 
-  * [Installation] 
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run: 
+
+  ${data.dependencies}
+
+  ## Usage
+
+  ${data.useRepo}
+
+  ## Contributing
+
+  ${data.contributeRepo}
+
+  ## Tests
+
+  Run ${data.tests} to run tests
+
+  ## Questions
+
+  What licenses should this project have?
+
+  ${data.license}
 
 `;
 }
